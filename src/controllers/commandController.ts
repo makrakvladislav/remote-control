50c1f1d2a36adff6768d3fea72534e42a30e2c7e
Vlad <makrak.vladislav@yandex.ru>
2023-01-23 19:54:56 +0300

    feat: add command controller

---
+++ src/controllers/commandController.ts
@@ -0,0 +1,30 @@
+import { ICommand } from 'src/helpers';
+import DrawingController from './drawingController';
+import NavigationController from './navigationController';
+import { prntScreen } from './screenController';
+
+export const controllers = async (data: ICommand) => {
+  if (data.command === 'mouse_up') {
+    await NavigationController.mouseUp(data);
+  } else if (data.command === 'mouse_down') {
+    await NavigationController.mouseDown(data);
+  } else if (data.command === 'mouse_left') {
+    await NavigationController.mouseLeft(data);
+  } else if (data.command === 'mouse_right') {
+    await NavigationController.mouseRight(data);
+  } else if (data.command === 'mouse_position') {
+    const pos = await NavigationController.mousePosition();
+    return `mouse_position ${pos.x},${pos.y}`;
+  } else if (data.command === 'draw_circle') {
+    await DrawingController.drawCircle(data);
+  } else if (data.command === 'draw_square') {
+    await DrawingController.drawSquare(data);
+  } else if (data.command === 'draw_rectangle') {
+    await DrawingController.drawRectangle(data);
+  } else if (data.command === 'prnt_scrn') {
+    const img = await prntScreen();
+    return `prnt_scrn ${img}`;
+  } else {
+    return `command not found`;
+  }
+};
